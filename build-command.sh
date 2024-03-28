5d35db2931e353e0848bc32855504497d04a9b29
张士杰 <2973707860@qq.com>
2024-03-28 15:49:21 +0800

    Update build-command.sh

--- build-command.sh
+++ build-command.sh
@@ -3,4 +3,4 @@ TAG=$1
 
 docker buildx build -t  myserver-cn-north-1.jcr.service.jdcloud.com/myserver/hello-world:${TAG} .
 
-#docker push myserver-cn-north-1.jcr.service.jdcloud.com/myserver/hello-world:${TAG}
+docker push myserver-cn-north-1.jcr.service.jdcloud.com/myserver/hello-world:${TAG}
