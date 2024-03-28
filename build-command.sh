742e2295c82dd863c3ad8ad3a4c28f5d1e58b193
zhangshijie <2973707860@qq.com>
2024-03-28 11:30:54 +0800

    main

---
+++ build-command.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+TAG=$1
+
+docker buildx build -t  myserver-cn-north-1.jcr.service.jdcloud.com/myserver/hello-world:${TAG} .
+
+#docker push myserver-cn-north-1.jcr.service.jdcloud.com/myserver/hello-world:${TAG}
